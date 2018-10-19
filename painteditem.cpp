58b3140b80a85c5f4e32787b854e381db698ced6
DBLTSGONNEO64U7\Administrator <2454878718@qq.com>
2018-10-19 11:26:48 +0800

    new item

--- painteditem.cpp
+++ painteditem.cpp
@@ -16,9 +16,10 @@ PaintedItem::PaintedItem(QQuickItem *parent)
     : QQuickPaintedItem(parent)
     , m_pen(Qt::red)
 {
-    setAcceptedMouseButtons(Qt::LeftButton);
+    //setAcceptedMouseButtons(Qt::LeftButton);
     qDebug()<<"PaintedItem::PaintedItem(QQuickItem *parent)";
-    controller->setView(this);
+    //controller->setView(this);
+    m_Stroke = nullptr;
 }
 
 PaintedItem::~PaintedItem()
@@ -26,35 +27,26 @@ PaintedItem::~PaintedItem()
     //purgePaintElements();
 }
 
-void PaintedItem::setStrokes(const Strokes &strokes)
+void PaintedItem::setStroke(Stroke *stroke)
 {
     //qDebug()<<"void PaintedItem::setStrokes(Strokes *strokes)"<<strokes.size();
-    m_Strokes = strokes;
+    m_Stroke = stroke;
     update();
 }
 
 void PaintedItem::paint(QPainter *painter)
 {
-    if(m_Strokes.empty())
+    if(m_Stroke == nullptr)
         return;
 
     painter->setRenderHint(QPainter::Antialiasing);
 
-    int size = m_Strokes.size();
-
-    qDebug()<<"void PaintedItem::paint(QPainter *painter)"<<size;
-
-    Stroke* stroke;
-    for(int i = 0;i<size;++i){
-        stroke = m_Strokes.at(i);
-
-        int point_num = stroke->arr->point.size();
-        for(int i=1; i<point_num; i++) {
-            m_pen.setColor(stroke->states);
-            m_pen.setWidth(5*stroke->arr->point[i].w);
-            painter->setPen(m_pen);
-            painter->drawLine(QPointF( stroke->arr->point[i-1].p.x,  stroke->arr->point[i-1].p.y), QPointF( stroke->arr->point[i].p.x,  stroke->arr->point[i].p.y));
-        }
+    int point_num = m_Stroke->arr->point.size();
+    for(int i=1; i<point_num; i++) {
+        m_pen.setColor(m_Stroke->states);
+        m_pen.setWidth(5*m_Stroke->arr->point[i].w);
+        painter->setPen(m_pen);
+        painter->drawLine(QPointF( m_Stroke->arr->point[i-1].p.x,  m_Stroke->arr->point[i-1].p.y), QPointF( m_Stroke->arr->point[i].p.x,  m_Stroke->arr->point[i].p.y));
     }
 }
 
